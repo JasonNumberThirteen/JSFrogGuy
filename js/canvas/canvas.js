daa993467910245a6e0f97a19d7ca3a3f4d1c8ef
JasonNumberThirteen <jasonxiii97@gmail.com>
2025-03-25 12:50:19 +0100

    Add resizing the game window by the given scale

--- js/canvas/canvas.js
+++ js/canvas/canvas.js
@@ -19,6 +19,9 @@ class Canvas {
 		canvas.id = GAME_WINDOW_CANVAS_ID;
 		canvas.width = GAME_WIDTH;
 		canvas.height = GAME_HEIGHT;
+		canvas.style.width = (GAME_WIDTH*GAME_SCREEN_SCALE) + "px";
+		canvas.style.height = "auto";
+		canvas.style.imageRendering = "pixelated";
 
 		return canvas;
 	}
--- js/constants.js
+++ js/constants.js
@@ -1,5 +1,6 @@
 const GAME_WIDTH = 256;
 const GAME_HEIGHT = 144;
+const GAME_SCREEN_SCALE = 3;
 
 const GAME_WINDOW_CANVAS_ID = "gameWindow";
 
