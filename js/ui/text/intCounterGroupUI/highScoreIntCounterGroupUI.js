508878a43c3eb76b79e05f9dd76ad94925029ab0
JasonNumberThirteen <jasonxiii97@gmail.com>
2025-03-31 13:14:30 +0200

    Refactor code in the high score integer counter group UI script

--- js/ui/text/intCounterGroupUI/highScoreIntCounterGroupUI.js
+++ js/ui/text/intCounterGroupUI/highScoreIntCounterGroupUI.js
@@ -1,5 +1,5 @@
 class HighScoreIntCounterGroupUI extends IntCounterGroupUI {
 	constructor() {
-		super("HI-SCORE", 5000, new Point(GAME_WINDOW_WIDTH - 8, 8), TEXT_ALIGNED_TO_RIGHT_KEY);
+		super(HIGH_SCORE_HEADER_TEXT, 0, new Point(GAME_WINDOW_WIDTH - 8, 8), TEXT_ALIGNED_TO_RIGHT_KEY);
 	}
 }
