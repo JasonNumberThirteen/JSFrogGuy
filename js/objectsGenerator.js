725285144bc989c12a6f90db99a1c913d0bcfdac
JasonNumberThirteen <jasonxiii97@gmail.com>
2025-04-01 15:07:12 +0200

    Add generating many vehicles in the same row

--- js/objectsGenerator.js
+++ js/objectsGenerator.js
@@ -2,11 +2,25 @@ class ObjectsGenerator {
 	createVehicles() {
 		const vehicles = [];
 
-		vehicles.push(new VehicleAnimatedSprite(VEHICLES_SPRITE_SHEET_FILENAME, new Point(124, 112), 0, 8, 8, 7, false));
-		vehicles.push(new VehicleAnimatedSprite(VEHICLES_SPRITE_SHEET_FILENAME, new Point(124, 104), 1, 8, 8, 8, true));
-		vehicles.push(new VehicleAnimatedSprite(VEHICLES_SPRITE_SHEET_FILENAME, new Point(124, 96), 2, 8, 8, 9, false));
-		vehicles.push(new VehicleAnimatedSprite(VEHICLES_SPRITE_SHEET_FILENAME, new Point(124, 88), 3, 8, 8, 10, true));
-		vehicles.push(new VehicleAnimatedSprite(VEHICLES_SPRITE_SHEET_FILENAME, new Point(124, 80), 2, 16, 8, 12, false));
+		for (let i = 0; i < 3; ++i) {
+			vehicles.push(new VehicleAnimatedSprite(VEHICLES_SPRITE_SHEET_FILENAME, new Point(124 + 40*i, 112), 0, 8, 8, 7, false));
+		}
+
+		for (let i = 0; i < 3; ++i) {
+			vehicles.push(new VehicleAnimatedSprite(VEHICLES_SPRITE_SHEET_FILENAME, new Point(160 + 32*i, 104), 1, 8, 8, 8, true));
+		}
+
+		for (let i = 0; i < 3; ++i) {
+			vehicles.push(new VehicleAnimatedSprite(VEHICLES_SPRITE_SHEET_FILENAME, new Point(80 + 32*i, 96), 2, 8, 8, 9, false));
+		}
+
+		for (let i = 0; i < 1; ++i) {
+			vehicles.push(new VehicleAnimatedSprite(VEHICLES_SPRITE_SHEET_FILENAME, new Point(68 + 32*i, 88), 3, 8, 8, 10, true));
+		}
+
+		for (let i = 0; i < 2; ++i) {
+			vehicles.push(new VehicleAnimatedSprite(VEHICLES_SPRITE_SHEET_FILENAME, new Point(124 + 40*i, 80), 2, 16, 8, 12, false));
+		}
 
 		return vehicles;
 	}
