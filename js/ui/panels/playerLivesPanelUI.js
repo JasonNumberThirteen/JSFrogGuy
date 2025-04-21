8a6d16090b71f6200dc9d5f33c35154510527ebc
JasonNumberThirteen <jasonxiii97@gmail.com>
2025-04-21 15:14:19 +0200

    Refactor code in the player lives panel UI script

--- js/ui/panels/playerLivesPanelUI.js
+++ js/ui/panels/playerLivesPanelUI.js
@@ -1,43 +1,56 @@
 class PlayerLivesPanelUI {
-	#group = [];
+	#sprites = [];
 	
 	constructor(numberOfSprites) {
 		this.#addSprites(numberOfSprites);
 	}
 
 	draw() {
-		this.#group.forEach(sprite => sprite.draw());
+		this.#sprites.forEach(sprite => sprite.draw());
 	}
 
 	setNumberOfSprites(numberOfSprites) {
-		const difference = this.#group.length - numberOfSprites;
+		const difference = this.#sprites.length - numberOfSprites;
 
 		if(difference > 0) {
-			this.#group.splice(difference - 1, difference);
+			this.#sprites.splice(difference - 1, difference);
 		} else if(difference < 0) {
-			this.#group.forEach(sprite => sprite.setX(sprite.getX() + Math.abs(difference)*12));
-			this.#addSprites(numberOfSprites - this.#group.length);
+			this.#shiftSprites(Math.abs(difference));
+			this.#addSprites(numberOfSprites - this.#sprites.length);
 		}
+	}
+
+	#shiftSprites(numberOfSlots) {
+		this.#sprites.forEach(sprite => {
+			const spriteX = sprite.getX();
+			const offset = numberOfSlots*this.#getOffsetBetweenSprites(sprite.getImage());
+			
+			sprite.setX(spriteX + offset);
+		});
 	}
 
 	#addSprites(numberOfSprites) {
 		for (let i = numberOfSprites; i >= 1; --i) {
-			this.#addSpriteToGroup(i);
+			this.#addSprite(i);
 		}
 	}
 
-	#addSpriteToGroup(ordinalNumber) {
+	#addSprite(ordinalNumber) {
 		const sprite = new Sprite(PLAYER_LIFE_SPRITE_FILENAME, undefined, sprite => this.#onSpriteLoad(sprite, ordinalNumber));
 
-		this.#group.push(sprite);
+		this.#sprites.push(sprite);
 	}
 
 	#onSpriteLoad(sprite, ordinalNumber) {
 		const image = sprite.getImage();
-		const offsetBetweenSprites = image.width + PLAYER_LIVES_SPRITES_GROUP_PANEL_UI_OFFSET_BETWEEN_SPRITES;
-		const x = PLAYER_LIVES_SPRITES_GROUP_PANEL_UI_OFFSET_FROM_EDGES + offsetBetweenSprites*(ordinalNumber - 1);
-		const y = GAME_WINDOW_HEIGHT - image.height - PLAYER_LIVES_SPRITES_GROUP_PANEL_UI_OFFSET_FROM_EDGES;
+		const offsetBetweenSprites = this.#getOffsetBetweenSprites(image)*(ordinalNumber - 1);
+		const x = PLAYER_LIVES_PANEL_UI_OFFSET_FROM_EDGES + offsetBetweenSprites;
+		const y = GAME_WINDOW_HEIGHT - image.height - PLAYER_LIVES_PANEL_UI_OFFSET_FROM_EDGES;
 		
 		sprite.setPosition(new Point(x, y));
+	}
+
+	#getOffsetBetweenSprites(image) {
+		return image.width + PLAYER_LIVES_PANEL_UI_OFFSET_BETWEEN_SPRITES
 	}
 }
