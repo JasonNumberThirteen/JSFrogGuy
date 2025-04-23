1d82d62171f9c29542701c147a5edae2bdc0cdb4
JasonNumberThirteen <jasonxiii97@gmail.com>
2025-04-23 21:47:33 +0200

    Fix modifier of the variable in the list methods script

--- js/staticMethods/listMethods.js
+++ js/staticMethods/listMethods.js
@@ -1,6 +1,6 @@
 class ListMethods {
 	static removeElementByReferenceIfPossible(list, element) {
-		var index = list.indexOf(element);
+		const index = list.indexOf(element);
 
 		if(index !== -1) {
 			list.splice(index, 1);
